c23204ab3c282c2d9c713aa5421316e6444d4dc9
Keith Burnell <keith@kburnell.com>
2018-07-25 11:02:52 -0500

    updated shirt mappings

--- utility/titoHelpers.js
+++ utility/titoHelpers.js
@@ -26,11 +26,20 @@ const titoHelper = () => {
   const bitShiftShirts = (shirt) => {
     let newShirt = shirt
 
+    //adjust for odd mismatches
+    if (shirt.toUpperCase().includes('MEN XX-LARGE'))
+      newShirt = 'Men 2X-Large';
+    else if (shirt.toUpperCase().includes('WOMEN\'S-CUT XX-LARGE'))
+      newShirt = 'Women\'s-cut 2X-Large'
+    else if (shirt.toUpperCase().includes('MEN XXX-LARGE'))
+      newShirt = 'Men 3X-Large'
+
+
+    //adjust for lack of availability from vendor
     if (shirt.toUpperCase().includes('Youth XS'.toUpperCase()))
       newShirt = 'Youth S     [6-8]' // hardcoded based on what is in tito, has to match
-
-    if (shirt.toUpperCase().includes('Youth XL'.toUpperCase()))
-      newShirt = 'Men X-Small' // hardcoded based on what is in tito, has to match
+    if (shirt.toUpperCase().includes('Women\'s-cut 4X-Large'.toUpperCase()))
+      newShirt = 'Men 4X-Large' // hardcoded based on what is in tito, has to match
 
     return newShirt
   }
